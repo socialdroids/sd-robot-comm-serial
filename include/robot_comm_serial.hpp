8cf289d8f762c4f117038fc2ccbb882a9b84b48e
Leonardo Costa <leosilvacosta6@gmail.com>
2025-06-10 19:31:39 -0300

    Recepção de dados em alta frequência - Recepção de dados ok, testado em 500Hz, processamento próximo de 10%

--- include/robot_comm_serial.hpp
+++ include/robot_comm_serial.hpp
@@ -1,9 +1,54 @@
 #ifndef INCLUDE_INCLUDE_ROBOT_COMM_SERIAL_HPP_
 #define INCLUDE_INCLUDE_ROBOT_COMM_SERIAL_HPP_
 
+#include <chrono>
+#include <rclcpp/executors.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <serial/serial.h>
-#include "feedback.pb.h"
 #include <string.h>
 
-#endif  // INCLUDE_INCLUDE_ROBOT_COMM_SERIAL_HPP_
+#include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/int32_multi_array.hpp"
+
+#include "command.pb.h"
+#include "feedback.pb.h"
+
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::microseconds;
+using std::chrono::milliseconds;
+using std::chrono::nanoseconds;
+
+class RobotSerial : public rclcpp::Node
+{
+public:
+    RobotSerial();
+    ~RobotSerial();
+
+private:
+    static constexpr char PACKET_END[] = {"\0"};
+    static constexpr size_t MAX_PACKET_SIZE{2048};
+    // Somente para exemplo
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr bumper_vel_pub_;
+    rclcpp::Subscription<std_msgs::msg::Int32MultiArray>::SharedPtr bumper_sub_;
+
+    std::shared_ptr<serial::Serial> serial_port_;
+    rclcpp::TimerBase::SharedPtr packet_timer_;
+    rclcpp::TimerBase::SharedPtr reconnect_timer_;
+
+    std::string buffer_;
+    float packet_frequency_;
+    std::chrono::time_point<high_resolution_clock> last_packet_time_;
+
+    unsigned long baud_;
+    std::string port_;
+    bool connected_;
+    void bumper_callback(const std_msgs::msg::Int32MultiArray::SharedPtr msg);
+    void packet_callback();
+    void reconnect_callback();
+
+    void connect();
+};
+
+#endif // INCLUDE_INCLUDE_ROBOT_COMM_SERIAL_HPP_
--- src/robot_comm_serial.cpp
+++ src/robot_comm_serial.cpp
@@ -1,18 +1,149 @@
 #include "robot_comm_serial.hpp"
+#include <rclcpp/logging.hpp>
 
-int main(int argc, char *argv[]) {
+RobotSerial::RobotSerial()
+    : Node("RobotSerial"), baud_(1000000), port_("/dev/ttyACM0"),
+      connected_(false)
+{
+    bumper_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
+        "/cmd_vel_bumpers", 30);
+    bumper_sub_ = this->create_subscription<std_msgs::msg::Int32MultiArray>(
+        "/robot_base/bumpers", 10,
+        std::bind(&RobotSerial::bumper_callback, this, std::placeholders::_1));
 
-  rclcpp::init(argc, argv);
+    connect();
 
-  unsigned long baud = 1000000;
-  std::string port("/dev/ttyACM0");
+    buffer_.reserve(MAX_PACKET_SIZE);
 
-  FeedbackMessage message;
-  serial::Serial my_serial(port, baud, serial::Timeout::simpleTimeout(1000));
-  // auto node = std::make_shared<Bumpers>();
-  // node->run();
-  rclcpp::shutdown();
-  return 0;
+    packet_timer_ =
+        this->create_wall_timer(std::chrono::milliseconds(1),
+                                std::bind(&RobotSerial::packet_callback, this));
+    reconnect_timer_ = this->create_wall_timer(
+        std::chrono::seconds(1),
+        std::bind(&RobotSerial::reconnect_callback, this));
+    last_packet_time_ = high_resolution_clock::now();
+    packet_frequency_ = 0;
+}
 
-  return 0;
+RobotSerial::~RobotSerial()
+{
+}
+
+void RobotSerial::bumper_callback(
+    const std_msgs::msg::Int32MultiArray::SharedPtr)
+{
+}
+
+void RobotSerial::packet_callback()
+{
+    if (!connected_)
+    {
+        RCLCPP_WARN(this->get_logger(), "Serial port disconnected!");
+        return;
+    }
+    if (serial_port_->available() > 0)
+    {
+        RCLCPP_INFO(this->get_logger(), "Data available! %ld",
+                    serial_port_->available());
+        size_t packet_size = 0;
+        buffer_.clear();
+
+        try
+        {
+            // process packet
+            FeedbackMessage message;
+            packet_size =
+                serial_port_->read(buffer_, serial_port_->available());
+
+            packet_frequency_ =
+                1e9 / (duration_cast<nanoseconds>(high_resolution_clock::now() -
+                                                  last_packet_time_))
+                          .count();
+
+            last_packet_time_ = high_resolution_clock::now();
+            std::cout << "Data = ";
+            for (size_t n = 0; n < packet_size; ++n)
+            {
+                unsigned int num = buffer_.c_str()[n];
+                std::cout << std::hex << num << std::dec << ", ";
+            }
+            std::cout << "\n";
+            RCLCPP_INFO(this->get_logger(), "%f) Data", packet_frequency_);
+
+            bool ok = message.ParseFromArray(buffer_.c_str(), packet_size);
+            if (ok)
+            {
+                RCLCPP_INFO(this->get_logger(), "Received protobuf message!");
+            }
+            else
+            {
+                RCLCPP_ERROR(this->get_logger(),
+                             "Failed to parse Protobuf message");
+            }
+        }
+        catch (serial::PortNotOpenedException& e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Failed to read data! Reason: %s.",
+                         e.what());
+        }
+        catch (serial::SerialException& e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Failed to read data! Reason: %s.",
+                         e.what());
+        }
+    }
+}
+
+void RobotSerial::reconnect_callback()
+{
+    if (!connected_)
+    {
+        RCLCPP_INFO(this->get_logger(), "Trying to reconnect...");
+        connect();
+    }
+}
+
+void RobotSerial::connect()
+{
+    try
+    {
+        serial_port_ = std::make_shared<serial::Serial>(
+            port_, baud_, serial::Timeout::simpleTimeout(10));
+    }
+    catch (serial::IOException& e)
+    {
+        serial_port_ = std::make_shared<serial::Serial>();
+        RCLCPP_INFO(this->get_logger(),
+                    "Failed to open serial port! Reason: %s.", e.what());
+    }
+    catch (serial::PortNotOpenedException& e)
+    {
+        serial_port_ = std::make_shared<serial::Serial>();
+        RCLCPP_INFO(this->get_logger(),
+                    "Failed to open serial port! Reason: %s.", e.what());
+    }
+
+    if (serial_port_)
+    {
+        serial_port_->setTimeout(10, 1, 0, 1, 0);
+        connected_ = serial_port_->isOpen();
+        RCLCPP_INFO(this->get_logger(),
+                    "Serial port info\nPort: %s\nBaud Rate: %ld", port_.c_str(),
+                    baud_);
+        RCLCPP_INFO(this->get_logger(), "Serial port connected? %d",
+                    connected_);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
+
+    auto robot_serial_node = std::make_shared<RobotSerial>();
+    rclcpp::spin(robot_serial_node);
+
+    // FeedbackMessage message;
+    rclcpp::shutdown();
+
+    return 0;
 }
