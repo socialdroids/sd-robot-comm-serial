f39f0339db593db100942a5fe34d77147a9ff522
Leonardo Costa <leosilvacosta6@gmail.com>
2026-08-06 12:20:14 -0300

    feat(log): 🔊 adiciona parâmetro de verbosidade de logs no launch

--- launch/serial.launch.py
+++ launch/serial.launch.py
@@ -1,20 +1,28 @@
 from launch import LaunchDescription
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    log_level_arg = DeclareLaunchArgument(
+                'log_level',
+                default_value='info',
+                description='Logging level (debug, info, warn, error, fatal)'
+        )
+
     node = Node(
             name='robot_comm_serial',
             package='robot_comm_serial',
             executable='robot_comm_serial',
             # namespace='robot_1',
             output='screen',
+                ros_arguments=['--log-level', LaunchConfiguration('log_level')],
             parameters=[
             PathJoinSubstitution([get_package_share_directory(
                 'robot_comm_serial'), 'config', 'robot_serial.yaml'])
             ]
     )
 
-    ld = LaunchDescription([node])
+    ld = LaunchDescription([log_level_arg, node])
     return ld
